3e554e1fc193bc412d0ae6ca11efef4864b6b911
Rémi Campistron <remi.campistron@fms-ea.com>
2025-10-20 15:40:54 +0200

    Structure de base de la classe

--- Grid.py
+++ Grid.py
@@ -2,7 +2,7 @@ class Grid:
     cols_headings = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     rows_headings = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    def __init__(self,ships,miss_list):
+    def __init__(self,ships=None,miss_list=None):
         self.ships = ships
         self.miss_list = miss_list
         pass
@@ -10,8 +10,8 @@ class Grid:
     def create_grid(self):
         pass
 
-    def create_rows(self):
+    def create_rows(self,rows_headings, cols_headings, ships, miss_list):
         pass
 
-    def create_col_headings(self):
+    def create_col_headings(self,cols_headings):
         pass
