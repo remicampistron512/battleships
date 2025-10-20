a36c73739016dfa6238569f5a6c4e2aa378cddce
Rémi Campistron <remi.campistron@fms-ea.com>
2025-10-20 13:51:26 +0200

    structure basique de la classe

---
+++ Grid.py
@@ -0,0 +1,17 @@
+class Grid:
+    cols_headings = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    rows_headings = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    def __init__(self,ships,miss_list):
+        self.ships = ships
+        self.miss_list = miss_list
+        pass
+
+    def create_grid(self):
+        pass
+
+    def create_rows(self):
+        pass
+
+    def create_col_headings(self):
+        pass
